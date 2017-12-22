ab3abea65778ceaee28340814da0f32e03c0e984
xxxx591 <250173319@qq.com>
2017-12-22 16:56:42 +0800

    day1

--- src/components/heard/heard.vue
+++ src/components/heard/heard.vue
@@ -13,7 +13,7 @@
                     active-text-color="#93e0ff">
                     <el-menu-item index="1">处理中心</el-menu-item>
                     <el-submenu index="2">
-                        <template slot="title">我的工作台1111111</template>
+                        <template slot="title">我的工作台1111211111</template>
                         <el-menu-item index="2-1">选项1</el-menu-item>
                         <el-menu-item index="2-2">选项2</el-menu-item>
                         <el-menu-item index="2-3">选项3</el-menu-item>
